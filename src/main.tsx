c2a672ddc2dee822c0f936bcd7bd8f4a9a95350f
Mohammed Kaif Qureshi <kaif.qr1@gmail.com>
2026-09-13 18:59:45 +0530

    Remove overpowering theme overlay from portfolio

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import PortfolioEnhancer from './portfolio-enhancer.tsx';
-import PortfolioThemePlus from './portfolio-theme-plus.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <PortfolioEnhancer />
-    <PortfolioThemePlus />
   </StrictMode>
 );
